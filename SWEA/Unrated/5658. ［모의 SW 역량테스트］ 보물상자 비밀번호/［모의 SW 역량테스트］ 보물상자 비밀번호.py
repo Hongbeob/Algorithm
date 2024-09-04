0e3e195fb29c8074424413921305047c6dae7220
Hongbeob <bup553@naver.com>
2024-09-05 01:20:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 143 ms, Memory: 47,500 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -8,7 +8,6 @@ for tc in range(1, T + 1):
         num.append(num.popleft())
         for X in range(0,N,N//4):
             tmp=list(num)
-            if int(''.join(tmp[X:X+N//4]),16) in result: continue
             result.add(int(''.join(tmp[X:X+N//4]),16))
     result=list(result)
     result.sort(reverse=True)
