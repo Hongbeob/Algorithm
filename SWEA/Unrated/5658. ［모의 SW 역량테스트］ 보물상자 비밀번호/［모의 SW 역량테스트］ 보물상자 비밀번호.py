546eab1b8c9693576cba6174994476e3196d0317
Hongbeob <bup553@naver.com>
2024-09-05 00:52:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 143 ms, Memory: 48,548 KB -BaekjoonHub

---
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -0,0 +1,14 @@
+from collections import deque
+T = int(input())
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    num=deque(input())
+    result=[]
+    for _ in range(N//4):
+        num.append(num.popleft())
+        for X in range(0,N,N//4):
+            tmp=list(num)
+            if int(''.join(tmp[X:X+N//4]),16) in result: continue
+            result.append(int(''.join(tmp[X:X+N//4]),16))
+    result.sort(reverse=True)
+    print(f'#{tc} {result[K-1]}')
