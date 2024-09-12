da016cd763b38472973a9ab520ac5f1b6c00b683
Hongbeob <bup553@naver.com>
2024-09-13 00:39:32 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 2,682 ms, Memory: 134,320 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -1,29 +1,22 @@
-from heapq import heappush, heappop
+def find_set(x):
+    if p[x] == x:
+        return x
+
+    p[x] = find_set(p[x])
+    return p[x]
 
 
-def prim(start):
-    heap=[]
-    MST=[0] * (N+1)
+def union(x, y):  # 대표자를 비교해서 같은 그래프인지 알아보자
+    root_x = find_set(x)
+    root_y = find_set(y)
 
-    sum_dist=0
+    if root_x == root_y:  # 같다면 같은 그래프다.
+        return
 
-    heappush(heap,(0,start))
-
-    while heap:
-        dist, c_island = heappop(heap)
-        if MST[c_island]:
-            continue
-
-        sum_dist += E * dist
-
-        for next in range(N-1):
-            next_island,next_dist = graph[c_island][next]
-            if MST[next_island]:
-                continue
-
-            heappush(heap,(next_dist,next_island))
-        MST[c_island] = 1
-    return round(sum_dist)
+    if root_x > root_y:  # 같지 않고 x 루트가 더 크면 x의 루트는 y의 루트로 바꾼다.
+        p[root_x] = root_y
+    else:
+        p[root_y] = root_x
 
 
 T = int(input())
@@ -34,11 +27,20 @@ for tc in range(1, T + 1):
     E = float(input())
     p = [i for i in range(N + 1)]
     position = list(zip(X, Y))
-    graph = [[] for _ in range(N+1)] #인접 리스트
+    graph = []
     for i in range(N):
-        for j in range(N):
-            if i==j:continue
+        for j in range(i + 1, N):
             x_sq = (position[i][0] - position[j][0]) ** 2
             y_sq = (position[i][1] - position[j][1]) ** 2
-            graph[i+1].append((j+1,x_sq + y_sq))
-    print(f'#{tc} {prim(1)}')
+            graph.append((i + 1, j + 1, x_sq + y_sq))  # 가중치
+    graph.sort(key=lambda x: x[2])
+    cnt = 0
+    total = 0
+    for start, end, dist in graph:
+        if find_set(start) != find_set(end):
+            cnt += 1
+            union(start, end)
+            total += (E * dist)
+            if cnt == N - 1:
+                break
+    print(f'#{tc} {round(total)}')
