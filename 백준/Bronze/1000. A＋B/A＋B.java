ac1a4434b6a5675ed1837a72120be0dc23754f14
Hongbeob <bup553@naver.com>
2024-12-08 01:08:04 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] numbers = input.split(" ");
+        int A = Integer.parseInt(numbers[0]);
+        int B = Integer.parseInt(numbers[1]);
         System.out.println(A+B);
     }
 }
